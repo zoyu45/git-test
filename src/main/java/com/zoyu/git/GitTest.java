a2c5d98aa27ba032466ae2cb5d81e5f64eaf3d54
zoyu <zoyu45@163.com>
2021-09-27 18:56:11 +0800

    first commit

---
+++ src/main/java/com/zoyu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zoyu.git;
+
+/**
+ * @author zoyu
+ * @description no
+ * @date 2021/9/27 18:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
