cd6c1def98df10cf686942085a14607939608de6
zoyu <zoyu45@163.com>
2021-09-27 18:58:21 +0800

    second commit

--- src/main/java/com/zoyu/git/GitTest.java
+++ src/main/java/com/zoyu/git/GitTest.java
@@ -8,5 +8,6 @@ package com.zoyu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
